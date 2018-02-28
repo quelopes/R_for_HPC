9407767389ff17acf8d9460c98a07bf37c1400d2
Guilherme Gall <gmgall@gmail.com>
2018-02-28 11:27:20 -0300

    Reescreve doParallel.R para fazer o mesmo teste que clusterSplit.R faz

--- foreach/doParallel.R
+++ foreach/doParallel.R
@@ -1,8 +1,11 @@
 #!/usr/bin/env Rscript
 
 # Exemplo do uso de foreach() com o backend paralelo doParallel
+#
+# Assim como clusterSplit.R, demonstra a penalidade que a comunicação pode impor
+# ao desempenho
 
-# Carregamento do pacote
+# Carregamento dos pacotes
 library(foreach)
 library(doParallel)
 
@@ -17,11 +20,36 @@ a <- seq(1, 10000, 1)
 b <- seq(10, 100000,10)
 c <- runif(10000)
 
-# Fazendo em paralelo (a[1]+b[1])^c[1], (a[2]+b[2])^c[2] ...
+# Mediremos o tempo necessário para processar em paralelo
+#
+# (a[1]+b[1])^c[1]
+# (a[2]+b[2])^c[2]
+# ...
 # (a[10000]+b[10000])^c[10000]
-out <- foreach(a, b, c) %dopar% {
+system.time(out <- foreach(a, b, c) %dopar% {
 	func(a, b, c)
-}
+})
+
+# Agora dividiremos as entradas numa quantidade de fatias igual ao tamanho do
+# cluster
+a_chunk <- clusterSplit(cl, a)
+b_chunk <- clusterSplit(cl, b)
+c_chunk <- clusterSplit(cl, c)
+
+# Mediremos o tempo necessário para processar em paralelo
+#
+# func(a_chunk[1], b_chunk[1], c_chunk[1])
+# func(a_chunk[2], b_chunk[2], c_chunk[2])
+# ...
+# func(a_chunk[40], b_chunk[40], c_chunk[40])
+system.time(out_chunks <- foreach(i=1:40) %dopar% {
+	        (a_chunk[[i]] + b_chunk[[i]])**c_chunk[[i]]
+})
+
+# A linha abaixo testa se a saída gerada pelas 2 abordagens são iguais
+#
+# stopifnot() gera um erro caso a expressão seja avaliada como falsa
+stopifnot(unlist(out) == unlist(out_chunks))
 
 # Para os workers
 stopCluster(cl)
