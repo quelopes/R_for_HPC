f61bd5753353d3a1fc7160b80950715a31b5dc51
raquele <quelopes@gmail.com>
2018-08-13 21:45:42 -0300

    changes speedup exemples

--- SpeedupWithoutPar/speedupWithoutPar.R
+++ SpeedupWithoutPar/speedupWithoutPar.R
@@ -1,6 +1,11 @@
 #!/usr/bin/env Rscript
 
-# === Benchmark de diversas estrategias de laco no R (Sem paralelismo) ===
+# *******************************************************************************
+# === Benchmark de diversas estrategias de laco no R (Sem paralelismo) ==========
+# === Semana do supercomputador Santos Dumont, LNCC Petrópolis ==================
+# === Professores: Raquel L. Costa e Guilherme Gall =============================
+# === Exemplos extraídos de: Strategies to Speedup R Code (Selva Prabhakaran) ===
+# *******************************************************************************
 
 pot = 4
 # Create the data frame
@@ -12,7 +17,9 @@ data <- data.frame (col1, col2, col3, col4)
 rm(list=c("col1","col2","col3","col4"))
 dim(data)
 
+# ================
 # === original ===
+# ================
 original = function(df){
   for (i in 1:nrow(df)) { # for every row
     if ((df[i, 'col1'] + df[i, 'col2'] + df[i, 'col3'] + df[i, 'col4']) > 4) { # check if > 4
@@ -21,9 +28,12 @@ original = function(df){
       df[i, 5] <- "lesser_than_4" # assign 5th column
     }
   }
+  df
 }
 
+# ================
 # === pre-aloc ===
+# ================
 preAloc = function(df){
   output <- character (nrow(df)) # initialize output vector
   for (i in 1:nrow(df)) {
@@ -33,10 +43,13 @@ preAloc = function(df){
       output[i] <- "lesser_than_4"
     }
   }
-  df$output
+  df$output = output 
+  df
 }  
 
+# ====================
 # === outside loop ===
+# ====================
 outloop = function(df){
   output <- character (nrow(df))
   condition <- (df$col1 + df$col2 + df$col3 + df$col4) > 4  # condition check outside the loop
@@ -48,37 +61,32 @@ outloop = function(df){
     }
   }
   df$output <- output
+  df
 }
 
-# === one condition ===
-oneCond = function(df){
-  output <- character(nrow(df))
-  condition <- (df$col1 + df$col2 + df$col3 + df$col4) > 4
-  for (i in (1:nrow(df))[condition]) {  # run loop only for true conditions
-    if (condition[i]) {
-      output[i] <- "greater_than_4"
-    } else {
-      output[i] <- "lesser_than_4"
-    }
-  }
-  df$output 
-}
-
+# ==============
 # === ifelse ===
+# ==============
 ifElse = function(df){
   output <- ifelse ((df$col1 + df$col2 + df$col3 + df$col4) > 4, "greater_than_4", "lesser_than_4")
   df$output <- output
+  df
 }
 
+# =============
 # === which ===
+# =============
 uiti = function(df){
   want = which(rowSums(df) > 4)
   output = rep("less than 4", times = nrow(df))
   output[want] = "greater than 4"
+  df$output = output
+  df
 }
 
-
+# ===================
 # === system.time ===
+# ===================
 print("=== Original ===")
 original = system.time({original(data)})
 original
@@ -88,9 +96,6 @@ preAloc
 print("=== outloop ===")
 outLoop = system.time({outloop(data)})
 outLoop
-print("=== oneCond ===")
-oneCond = system.time({oneCond(data)})
-oneCond
 print("=== ifElse ===")
 ifElse = system.time({ifElse(data)})
 ifElse
@@ -98,5 +103,5 @@ print("=== which ===")
 uiti = system.time({uiti(data)})
 uiti
 
-print(rbind(original,preAloc,outLoop,oneCond,ifElse,uiti))
+print(rbind(original,preAloc,outLoop,ifElse,uiti))
 
