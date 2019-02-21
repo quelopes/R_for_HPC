da62c584ab987c2960ac77359cf74cc16b8a9ca0
raquele <quelopes@gmail.com>
2019-02-21 16:52:22 -0300

    update pacoteInstalacao.R

--- InstalacaoPacotes.R
+++ InstalacaoPacotes.R
@@ -1,5 +1,5 @@
 list.of.packages = c("ggplot2","devtools","doParallel","doSNOW","foreach","snow","mlbench","caret","glmnet",
-"klaR","gbm")
+"klaR","gbm","c50","kernlab","randomForest","e1071")
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
